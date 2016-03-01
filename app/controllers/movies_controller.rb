4fd06d5a0c0203dfa875e46bbb5eab046b32a878
liuliu0510 <fangshuo0510@tamu.edu>
2016-03-01 22:04:03 +0000

    hw 2 part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,10 +11,21 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #@movies = Movie.find(:all, :order => (params[:sort_by]))
-   # @sort_column = params[:sort_by]
+   # @movies = Movie.find(:all, :order => (params[:sort_by]))
+  
     @sort_column = params[:sort_by]
+
     @movies= params[:sort_by]==nil ?  Movie.all : Movie.order("#{params[:sort_by]} ASC")
+    
+    if params[:ratings]
+        @movies = Movie.where(:rating => params[:ratings].keys)
+    end
+
+   @all_ratings = Movie.all_ratings
+   @set_ratings = params[:ratings]
+   if !@set_ratings
+     @set_ratings = Hash.new
+   end
     #@title_class=sort_column=="title" ? "hilite":""
    # @release_date_class=sort_column=="release_date" ? "hilite":""
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+    #attr_accessible :title, :rating, :description, :release_date
+    def self.all_ratings
+        a = Array.new
+        self.select("rating").uniq.each {|i| a.push(i.rating)}
+        a.sort.uniq
+    end
 end
